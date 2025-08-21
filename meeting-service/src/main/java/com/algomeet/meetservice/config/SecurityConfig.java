5c874e742b538498f4c039b7beb89f0ce4957325
sandroalgoframe <sandroalgoframe@gmail.com>
2025-08-21 11:35:21 +0530

    feat(meeting): Cors error fix

--- meeting-service/src/main/java/com/algomeet/meetservice/config/SecurityConfig.java
+++ meeting-service/src/main/java/com/algomeet/meetservice/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -33,4 +38,32 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        //TODO: For Prod Allow/restrict only intended
+        // Allowed origins
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000",
+                "https://localhost:8080",
+                "http://localhost:8080",   // all localhost ports
+                "https://*.algoframe.in" // any subdomain of algoframe.in
+        ));
+
+        // Allowed methods
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // Allowed headers
+        config.setAllowedHeaders(List.of("*"));
+
+        // Allow cookies / Authorization headers if needed
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
