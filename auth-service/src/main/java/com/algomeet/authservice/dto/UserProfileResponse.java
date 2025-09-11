aa60b25c1fdc73ba089f5dcaa035c8f2695851d2
maddox.algoframe <maddox.algoframe@gmail.com>
2025-09-11 15:13:59 +0800

    Fix sudden issue no args constructor required

--- auth-service/src/main/java/com/algomeet/authservice/dto/UserProfileResponse.java
+++ auth-service/src/main/java/com/algomeet/authservice/dto/UserProfileResponse.java
@@ -7,10 +7,12 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserProfileResponse {
     private UUID id;
     private Short loginTypePolicy;
