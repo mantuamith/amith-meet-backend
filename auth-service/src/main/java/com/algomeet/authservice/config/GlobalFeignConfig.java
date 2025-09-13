e636ba8ed80c3d380b168d4cd270252fb9c963e7
maddox.algoframe <maddox.algoframe@gmail.com>
2025-09-13 11:59:24 +0800

    Add global feign decoder for auth, to ignore feign DTO response unknown fields

---
+++ auth-service/src/main/java/com/algomeet/authservice/config/GlobalFeignConfig.java
@@ -0,0 +1,30 @@
+package com.algomeet.authservice.config;
+
+import java.util.Collections;
+
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+import feign.codec.Decoder;
+
+@Configuration
+public class GlobalFeignConfig {
+
+	/**
+	 * Used to ignore feign DTO response unknown fields
+	 * @return
+	 */
+    @Bean
+    public Decoder feignDecoder() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.getObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return new ResponseEntityDecoder(new SpringDecoder(() -> new HttpMessageConverters(Collections.singletonList(converter))));
+    }
+}
