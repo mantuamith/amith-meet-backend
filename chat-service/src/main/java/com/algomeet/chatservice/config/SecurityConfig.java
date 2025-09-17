dc796a43ede3f029331131d49542ae11dc735657
sonaialgoframe <sonai.algoframe@gmail.com>
2025-09-17 19:14:48 +0530

    Fix Cors

--- chat-service/src/main/java/com/algomeet/chatservice/config/SecurityConfig.java
+++ chat-service/src/main/java/com/algomeet/chatservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.algomeet.chatservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/chat-ws/**").permitAll()
--- contact-service/src/main/java/com/algomeet/contactservice/config/SecurityConfig.java
+++ contact-service/src/main/java/com/algomeet/contactservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.algomeet.contactservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/contacts/**").authenticated()
