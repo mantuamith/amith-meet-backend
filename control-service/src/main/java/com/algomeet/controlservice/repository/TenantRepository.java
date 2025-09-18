a8ff672e3a578f3d165988ad5aff7fda66fcdc15
maddox.algoframe <maddox.algoframe@gmail.com>
2025-09-18 20:01:57 +0800

    Add to support multi-tenant

--- control-service/src/main/java/com/algomeet/controlservice/repository/TenantRepository.java
+++ control-service/src/main/java/com/algomeet/controlservice/repository/TenantRepository.java
@@ -1,8 +1,20 @@
 package com.algomeet.controlservice.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algomeet.controlservice.entity.Tenant;
+import com.algomeet.multitenancy.annotations.UsePublicSchema;
 
 public interface TenantRepository extends JpaRepository<Tenant, Integer> {
+	@UsePublicSchema
+	<S extends Tenant> S save(S entity);
+	
+	@UsePublicSchema
+	Optional<Tenant> findById(Integer id);
+	
+	@UsePublicSchema
+	List<Tenant> findAll();
 }
