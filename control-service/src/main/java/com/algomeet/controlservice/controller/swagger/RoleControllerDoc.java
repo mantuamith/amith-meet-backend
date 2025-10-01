64d0f0dbeb2f08b576087fede1991d1a24720661
maddox.algoframe <maddox.algoframe@gmail.com>
2025-10-01 14:03:56 +0800

    Remove unused imports

--- control-service/src/main/java/com/algomeet/controlservice/controller/swagger/RoleControllerDoc.java
+++ control-service/src/main/java/com/algomeet/controlservice/controller/swagger/RoleControllerDoc.java
@@ -3,12 +3,7 @@ package com.algomeet.controlservice.controller.swagger;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.algomeet.controlservice.dto.CommonResponse;
