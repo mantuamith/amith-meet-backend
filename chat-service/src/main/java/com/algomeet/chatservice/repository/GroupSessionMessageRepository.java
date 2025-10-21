80823de6fdb0c0dd526d19b89e0e11e432449739
maddox.algoframe <maddox.algoframe@gmail.com>
2025-10-21 13:23:57 +0800

    Update method names

--- chat-service/src/main/java/com/algomeet/chatservice/repository/GroupSessionMessageRepository.java
+++ chat-service/src/main/java/com/algomeet/chatservice/repository/GroupSessionMessageRepository.java
@@ -7,16 +7,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.algomeet.chatservice.document.GroupSessionMessageDocument;
 import com.algomeet.chatservice.document.MessageDocument;
-import com.algomeet.chatservice.model.MessageStatus;
 
 public interface GroupSessionMessageRepository extends MongoRepository<GroupSessionMessageDocument, String> {
 	void deleteByCorrelationId(String correlationId);
 	
-    List<MessageDocument> findByReceiver(String receiver, Pageable pageable);
+    List<MessageDocument> findByTo(String receiver, Pageable pageable);
 
-    List<MessageDocument> findByReceiver(String receiver);   
-
-    List<MessageDocument> findByReceiverAndStatus(String receiver, MessageStatus status);
-    
-    
+    List<MessageDocument> findByTo(String receiver);      
 }
