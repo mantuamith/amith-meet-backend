82cbd306283bdd07401ba16c4fd32bfa36eeab6e
sandroalgoframe <sandroalgoframe@gmail.com>
2025-11-11 12:27:11 +0530

    Check case insensitivenes

--- user-service/src/main/java/com/algomeet/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/algomeet/userservice/repository/UserRepository.java
@@ -2,10 +2,6 @@ package com.algomeet.userservice.repository;
 
 import com.algomeet.multitenancy.annotations.UsePublicSchema;
 import com.algomeet.userservice.model.User;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,56 +11,33 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
-	@UsePublicSchema
-	<S extends User> S save(S entity);
-	
-	@UsePublicSchema
-	Optional<User> findById(Long id);
-	
-	@UsePublicSchema
     Optional<User> findByUsername(String username);
 
-	@UsePublicSchema
     Optional<User> findByEmail(String email);
 
-	@UsePublicSchema
     List<User> findAllByEmailIn(List<String> emails);
 
-	@UsePublicSchema
     @Query("SELECT u FROM User u WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(u.email) LIKE LOWER(CONCAT('%', :query, '%'))")
     List<User> searchUsers(@Param("query") String query);
 
     //boolean deleteUserByEmail(String email);
 
-	@UsePublicSchema
-    boolean existsByEmailIgnoreCase(String email);
+    boolean existsByEmail(String email);
 
-	@UsePublicSchema
-    void deleteByEmailIgnoreCase(String email);
+    void deleteByEmail(String email);
 
-	@UsePublicSchema
-    boolean existsByUsernameIgnoreCase(String username);
+    boolean existsByUsername(String username);
 
-	@UsePublicSchema
     boolean existsByPhone(String phone);
 
-	@UsePublicSchema
     Optional<User> findByUsernameIgnoreCase(String username);
-	
-	@UsePublicSchema
     Optional<User> findByEmailIgnoreCase(String email);
     
     @UsePublicSchema
     Optional<User> findByUserKey(UUID key);
 
-    @UsePublicSchema
     List<User> findAllByUserKeyIn(List<UUID> keys);
 
-    @UsePublicSchema
     List<User> findByUserKeyIn(Collection<UUID> keys);
-    
-    @UsePublicSchema
-    Page<User> findAll(Specification<User> spec, Pageable pageable);
 }
