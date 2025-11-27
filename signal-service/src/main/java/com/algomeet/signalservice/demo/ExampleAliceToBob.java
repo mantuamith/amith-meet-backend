91357c540983955a2e5abc258f6f25580f7753a5
maddox.algoframe <maddox.algoframe@gmail.com>
2025-11-27 21:01:54 +0800

    Update comments

--- signal-service/src/main/java/com/algomeet/signalservice/demo/ExampleAliceToBob.java
+++ signal-service/src/main/java/com/algomeet/signalservice/demo/ExampleAliceToBob.java
@@ -83,6 +83,7 @@ public class ExampleAliceToBob {
 		// This endoint will return the device ID save it in your device to be used in succeeding request.
 		
 		// Get/Retrieve recipient device identity key from backend using API endpoint: GET /signal/v2/devices?userKey=2fc35cae-e0b7-40a5-b2aa-e86206730e99
+		// Get/Retrieve one-time prekey using this endoint: GET /signal/v2/devices/{deviceId}/prekeys/one-time?userKey=XXX
 		PreKeyBundle bobPreKeyBundle= new PreKeyBundle(bobRegistrationId, 
 				bobDeviceId,
 				bobPreKeyId, 
