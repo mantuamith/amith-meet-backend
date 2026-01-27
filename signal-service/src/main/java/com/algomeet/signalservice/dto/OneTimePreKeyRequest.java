82c88fc92459b425fb4aa7fe3430405d176ad20f
sandroalgoframe <sandroalgoframe@gmail.com>
2026-01-27 12:21:28 +0530

    Signal pre-key ID for one time pre key request. Change to Long from Int... Since IOS uses pre crpto lib to genearte and it is value exceeds range of Integer.

--- signal-service/src/main/java/com/algomeet/signalservice/dto/OneTimePreKeyRequest.java
+++ signal-service/src/main/java/com/algomeet/signalservice/dto/OneTimePreKeyRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class OneTimePreKeyRequest {	
 	@Min(value = 1, message = "preKeyId must be greater than 0")
-	private Integer preKeyId;
+	private Long preKeyId;
 	
 	@NotEmpty
 	private String publicKey;
