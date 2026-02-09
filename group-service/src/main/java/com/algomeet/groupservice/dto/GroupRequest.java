f80cde51c0014ba0b7a52f746c50f9b6abd37f7e
maddox.algoframe <maddox.algoframe@gmail.com>
2026-02-09 21:21:29 +0800

    Add flexibility on creating empty room.

--- group-service/src/main/java/com/algomeet/groupservice/dto/GroupRequest.java
+++ group-service/src/main/java/com/algomeet/groupservice/dto/GroupRequest.java
@@ -3,13 +3,11 @@ package com.algomeet.groupservice.dto;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class GroupRequest {
 	private String name;
 	
-	@NotEmpty
 	private Set<MemberRequest> members = new HashSet<>();
 }
