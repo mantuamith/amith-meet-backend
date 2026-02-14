ef045aecfc378f34a286d8147b1295e7faa0fa8e
maddox.algoframe <maddox.algoframe@gmail.com>
2026-02-14 20:43:42 +0800

    Update response object

--- media-service/src/main/java/com/algomeet/mediaservice/controller/InternalUserStorageUsageController.java
+++ media-service/src/main/java/com/algomeet/mediaservice/controller/InternalUserStorageUsageController.java
@@ -2,19 +2,16 @@ package com.algomeet.mediaservice.controller;
 
 import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algomeet.mediaservice.dto.CommonResponse;
 import com.algomeet.mediaservice.dto.StorageUsageAdjustmentRequest;
 import com.algomeet.mediaservice.dto.StorageUsageResponse;
-import com.algomeet.mediaservice.enums.ResponseCode;
 import com.algomeet.mediaservice.service.impl.UserStorageUsageService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,35 +24,23 @@ public class InternalUserStorageUsageController {
     private final UserStorageUsageService service;
     
     @GetMapping("/{userKey}/storage-usage")
-	public ResponseEntity<CommonResponse<StorageUsageResponse>> getUserStorageUsage(@PathVariable("userKey") UUID userKey) {
-		StorageUsageResponse response = service.getUsage(userKey);
-
-        return ResponseEntity.ok(
-                CommonResponse.from(ResponseCode.SUCCESS, response)
-        );
+	public StorageUsageResponse getUserStorageUsage(@PathVariable("userKey") UUID userKey) {
+    	return service.getUsage(userKey);
 	}	
     
     /**
      * Adjusts user storage counters (increase/decrease).
      */
-    @PatchMapping("/{userKey}/storage-usage")
-    public ResponseEntity<CommonResponse<StorageUsageResponse>> adjustStorageUsage(
+    @PutMapping("/{userKey}/storage-usage")
+    public StorageUsageResponse adjustStorageUsage(
             @PathVariable UUID userKey,
             @RequestBody StorageUsageAdjustmentRequest request) {
 
-        StorageUsageResponse response = service.adjustUsage(userKey, request);
-
-        return ResponseEntity.ok(
-                CommonResponse.from(ResponseCode.SUCCESS, response)
-        );
+        return service.adjustUsage(userKey, request);
     }
     
     @DeleteMapping("/{userKey}/storage-usage")
-	public ResponseEntity<CommonResponse<?>> deleteUserStorageUsage(@PathVariable("userKey") UUID userKey) {
+	public void deleteUserStorageUsage(@PathVariable("userKey") UUID userKey) {
 		service.deleteUsage(userKey);
-
-        return ResponseEntity.ok(
-                CommonResponse.from(ResponseCode.SUCCESS)
-        );
 	}	
 }
