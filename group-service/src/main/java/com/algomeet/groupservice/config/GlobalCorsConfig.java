26b7743ef6cfac1e1d35504b1300d58e2a9b423d
sandroalgoframe <sandroalgoframe@gmail.com>
2026-04-15 16:07:38 +0530

    feat: expand CORS allowed origins across services
    - Added `*.algomeet.app` and `*.bhmeet.app` as allowed origins in `SecurityConfig` for XMPP, contact, and meeting services.
    - Introduced `CorsConfigurationSource` for flexible CORS management in group service.

--- group-service/src/main/java/com/algomeet/groupservice/config/GlobalCorsConfig.java
+++ group-service/src/main/java/com/algomeet/groupservice/config/GlobalCorsConfig.java
@@ -3,8 +3,13 @@ package com.algomeet.groupservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class GlobalCorsConfig {
@@ -20,6 +25,36 @@ public class GlobalCorsConfig {
                         .allowCredentials(false); // only true if you’re using cookies or auth headers
             }
         };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        //TODO: For Prod Allow/restrict only intended
+        // Allowed origins
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000",
+                "https://localhost:8080",
+                "http://localhost:8080",   // all localhost ports
+                "https://*.algoframe.in",
+                "https://*.algomeet.app",
+                "https://*.bhmeet.app"
+                // any subdomain of algoframe.in
+        ));
+
+        // Allowed methods
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // Allowed headers
+        config.setAllowedHeaders(List.of("*"));
+
+        // Allow cookies / Authorization headers if needed
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
 
