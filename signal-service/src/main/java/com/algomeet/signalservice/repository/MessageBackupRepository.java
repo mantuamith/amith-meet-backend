d6b83ba91d54d6215bb7a619dc13d6649d5fe9be
maddox.algoframe <maddox.algoframe@gmail.com>
2026-05-04 19:17:39 +0800

    remove unused code

--- signal-service/src/main/java/com/algomeet/signalservice/repository/MessageBackupRepository.java
+++ signal-service/src/main/java/com/algomeet/signalservice/repository/MessageBackupRepository.java
@@ -14,15 +14,7 @@ import com.algomeet.signalservice.repository.projection.ConversationStorageStats
 
 import jakarta.transaction.Transactional;
 
-public interface MessageBackupRepository extends MongoRepository<MessageBackupDocument, String> {
-	
-	@Deprecated
-	@Query("{ '$or': [ " +
-			"{ 'userKey': ?0, 'senderKey': ?0, 'receiverKey': ?1 }, " +
-			"{ 'userKey': ?0, 'senderKey': ?1, 'receiverKey': ?0 } " +
-			"] }")
-	Page<MessageBackupDocument> findConversation(String userA, String userB, Pageable pageable);
-	
+public interface MessageBackupRepository extends MongoRepository<MessageBackupDocument, String> {	
 	Page<MessageBackupDocument> findByConversationId(
 			String conversationId, Pageable pageable);
 	
