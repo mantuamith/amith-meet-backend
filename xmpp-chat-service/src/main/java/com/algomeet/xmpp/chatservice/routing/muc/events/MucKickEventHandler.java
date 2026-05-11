fe83fab932dc1aab4fb3d229885611bfdca95044
maddox.algoframe <maddox.algoframe@gmail.com>
2026-05-11 16:09:03 +0800

    Update target

--- xmpp-chat-service/src/main/java/com/algomeet/xmpp/chatservice/routing/muc/events/MucKickEventHandler.java
+++ xmpp-chat-service/src/main/java/com/algomeet/xmpp/chatservice/routing/muc/events/MucKickEventHandler.java
@@ -93,7 +93,7 @@ public class MucKickEventHandler {
         		senderJid,
 				roomBareJid,
 				body,
-				senderJid);
+				victimJid);
 
 		/**
 		 * ----------------------------------------------------------
