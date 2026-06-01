fc0ad4b6f0c763251cb7fdcb347ed047f3a584da
maddox.algoframe <maddox.algoframe@gmail.com>
2026-06-01 13:47:03 +0800

    Refactor field to typo error when used in MongoTemplate

--- xmpp-chat-service/src/main/java/com/algomeet/xmpp/chatservice/document/CallSession.java
+++ xmpp-chat-service/src/main/java/com/algomeet/xmpp/chatservice/document/CallSession.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import com.algomeet.xmpp.chatservice.enums.CallStatus;
 import com.algomeet.xmpp.chatservice.enums.ParticipantCallStatus;
@@ -33,37 +34,73 @@ import com.algomeet.xmpp.chatservice.enums.ParticipantCallStatus;
 })
 
 public class CallSession {
+	public static final String ID = "id";
+	public static final String SID = "sid";
+	public static final String CALLER = "caller";
+	public static final String CALLER_SID = "callerSid";
+	public static final String CALLEE = "callee";
+	public static final String CALLEE_SID = "calleeSid";
+	public static final String CALL_TYPE = "callType";
+	public static final String ROOM_ID = "roomId";
+	public static final String STATUS = "status";
+	public static final String CALLER_STATUS = "callerStatus";
+	public static final String CALLEE_STATUS = "calleeStatus";
+	public static final String TENANT_ID = "tenantId";
+	public static final String CREATED_AT = "createdAt";
+	public static final String ACCEPTED_AT = "acceptedAt";
+	public static final String TERMINATED_AT = "terminatedAt";
+	public static final String UPDATED_AT = "updatedAt";
+	
 	@Id
 	private String id;
 
 	@Indexed
+	@Field(SID)
 	private String sid;       // Maps to your Jingle 'sid'
 
 	@Indexed
+	@Field(CALLER)
 	private UUID caller;    // caller user key
 
 	@Indexed
+	@Field(CALLER_SID)
 	private String callerSid; // caller websocket connection session ID
 
 	@Indexed
+	@Field(CALLEE)
 	private UUID callee;    // callee user key
 
 	@Indexed
+	@Field(CALLEE_SID)
 	private String calleeSid;  // callee websocket connection session ID
 
+	@Field(CALL_TYPE)
 	private String callType;  // "audio" or "video"
 
+	@Field(ROOM_ID)
 	private UUID roomId;    // Room/Group chat Id
 
+	@Field(STATUS)
 	private CallStatus status;
 
+	@Field(CALLER_STATUS)
 	private ParticipantCallStatus callerStatus;
 
+	@Field(CALLEE_STATUS)
 	private ParticipantCallStatus calleeStatus;
 
+	@Field(TENANT_ID)
 	private Integer tenantId;
 
+	@Field(CREATED_AT)
 	private Long createdAt;    // When the call invitation was sent
+
+	@Field(ACCEPTED_AT)
 	private Long acceptedAt;   // When the callee clicked "Answer"
+
+	@Field(TERMINATED_AT)
 	private Long terminatedAt; // When the call ended (success/cancel/error)
+
+	@Field(UPDATED_AT)
+	private Long updatedAt;    // Track structural modification/state changes	
 }
