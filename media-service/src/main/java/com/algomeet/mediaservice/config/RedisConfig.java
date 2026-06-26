3b0cb6567b9595d538786c32fd23f1b75be05fe8
sandroalgoframe <sandroalgoframe@gmail.com>
2026-06-26 17:37:06 +0530

    fix: mark streamStringRedisTemplate as @Primary to resolve ambiguous RedisTemplate injection

--- media-service/src/main/java/com/algomeet/mediaservice/config/RedisConfig.java
+++ media-service/src/main/java/com/algomeet/mediaservice/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.algomeet.mediaservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -14,33 +15,28 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
-	
-	@Bean
+
+    @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // Use JSON for values - much faster and more compact than JDK default
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
-        
+
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(serializer);
-        
+
         return template;
     }
-	
-	@Bean    
+
+    @Primary
+    @Bean
     public RedisTemplate<String, String> streamStringRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        /**
-         * Use plain string serialization for both channels and payloads.
-         *
-         * Faster than JSON serializers for Pub/Sub transport messaging.
-         */
         StringRedisSerializer serializer = new StringRedisSerializer();
 
         template.setKeySerializer(serializer);
