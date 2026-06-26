2151eabdd33d8deddb53fb05580b9b8c36744a00
sandroalgoframe <sandroalgoframe@gmail.com>
2026-06-26 17:54:06 +0530

    fix: add @Primary notificationStringRedisTemplate to resolve ambiguous RedisTemplate injection

--- notification-service/src/main/java/com/algomeet/notificationservice/config/PushMessageRedisConfig.java
+++ notification-service/src/main/java/com/algomeet/notificationservice/config/PushMessageRedisConfig.java
@@ -3,6 +3,7 @@ package com.algomeet.notificationservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -14,19 +15,20 @@ import com.algomeet.notificationservice.subscriber.PushMessageSubscriber;
 
 @Configuration
 public class PushMessageRedisConfig {
-	@Value("${redis.notification.push-message-channel:notification-push-message-channel}")
-	private String pushMessageChannel;
- 
+
+    @Value("${redis.notification.push-message-channel:notification-push-message-channel}")
+    private String pushMessageChannel;
+
     @Bean
     public ChannelTopic topic() {
         return new ChannelTopic(pushMessageChannel);
     }
-    
+
     @Bean
     public MessageListenerAdapter listenerAdapter(PushMessageSubscriber subscriber) {
         return new MessageListenerAdapter(subscriber, "onMessage");
     }
-    
+
     @Bean
     public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
                                                         MessageListenerAdapter listenerAdapter) {
@@ -35,18 +37,34 @@ public class PushMessageRedisConfig {
         container.addMessageListener(listenerAdapter, topic());
         return container;
     }
-    
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-        
-        // Set serializer
+
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());  // For plain text messages
-        
+        template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Primary
+    @Bean
+    public RedisTemplate<String, String> notificationStringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer serializer = new StringRedisSerializer();
+
+        template.setKeySerializer(serializer);
+        template.setValueSerializer(serializer);
+        template.setHashKeySerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+
         return template;
     }
 }
