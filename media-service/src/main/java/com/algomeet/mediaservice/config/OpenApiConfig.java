e4ddaabb000c1cf970004e245ab8477faba1d0b9
sonaialgoframe <sonai.algoframe@gmail.com>
2026-06-30 13:48:55 +0530

    fix: fix media-service OpenApiConfig server URL and align bearerAuth scheme

--- media-service/src/main/java/com/algomeet/mediaservice/config/OpenApiConfig.java
+++ media-service/src/main/java/com/algomeet/mediaservice/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.algomeet.mediaservice.config;
 
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -15,18 +13,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("File Sharing API")
-                        .description("Management of the file sharing feature")
-                        .version("1.0.0"))
-                .addServersItem(new Server()
-                        .url("https://your-server-here")
-                        .description("Generated server url"))
-                .components(new Components().addSecuritySchemes("HttpBearerKey",
+                .components(new Components().addSecuritySchemes("bearerAuth",
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .description("Http request Authorization header")))
-                .addSecurityItem(new SecurityRequirement().addList("HttpBearerKey"));
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
